73f0931d6c255091c7a2bb99e499aec68b6a6fc5
yichenghul <1472434868@qq.com>
2021-01-12 14:37:03 +0800

    添加 ant motion logo 动画

---
+++ src/pages/login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Logo from './../../components/logo/logo';
+
+export default class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Logo />
+      </div>
+    )
+  }
+}
